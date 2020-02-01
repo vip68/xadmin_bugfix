497b349a74faa35538f1b7568b3db36ec2652009
flyskyer <vip68@msn.com>
2020-02-02 00:12:50 +0800

    更新demo

--- demo/apps/base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from base.views import set_homepage
-
-app_name = 'base'
-
-urlpatterns = [
-    path('homepage', set_homepage, name='homepage'),
-]
--- demo/apps/service/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .article import Article, ReleaseLog
-from .urlslink import UrlsLink, AppUrlLink
